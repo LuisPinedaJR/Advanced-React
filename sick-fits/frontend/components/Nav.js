01962e3036dc2cdd6df6f0a3b4ef9fd33fdfc0fa
Luis Pineda <lspnd00@gmail.com>
2020-09-08 21:47:35 -0400

    setup frontend for store

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,16 @@
+const { func } = require('prop-types')
+
+import Link from 'next/link'
+
+const Nav = () => (
+  <div>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+)
+
+export default Nav
